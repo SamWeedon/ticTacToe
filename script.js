f5d4881527c402c2537dc537c5fe86b3bcc14ca5
SamWeedon <64287633+SamWeedon@users.noreply.github.com>
2023-06-18 21:20:46 -0600

    Add basic gameboard display functionality

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const gameBoard = (() => {
-    let moves = [];
+    let moves = ['x','o','x',
+                 'o','x','o',
+                 'o','x','o'];
+    const boxes = document.querySelectorAll('.box');
+    let index = 0;
+    const displayMoves = () => {
+        boxes.forEach(function(box) {
+            box.textContent = moves[index];
+            index++;
+        })
+    }
+    return {displayMoves, moves};
 })();
 
 const displayController = (() => {
@@ -9,3 +20,6 @@ const displayController = (() => {
 const Player = () => {
 
 };
+
+//driver script
+gameBoard.displayMoves();
